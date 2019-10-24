54b8f66b81f870d44c6022a990cdb100330cac6b
Gia Khanh <giakhanh9890@gmail.com>
2019-10-24 23:21:22 +0700

    Khánh fix linh tinh

--- admin/quantritaikhoanuser.php
+++ admin/quantritaikhoanuser.php
@@ -42,7 +42,7 @@
                         </a>
                     </li>  
                     <li class="nav-item">
-                        <a class="nav-link" href="quantriphanhoi.php" >
+                        <a class="nav-link" href="quantritaikhoanuser.php" >
                         Quản trị tài khoản người dùng
                         </a>
                     </li>  
