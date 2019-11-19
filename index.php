d985d632d0812d53d744106263d41743b435bb3b
minh khai le <khailm@allogical.com>
2019-11-19 09:29:56 +0700

    khai

--- index.php
+++ index.php
@@ -78,8 +78,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							echo '<div class="col-md-3 " style="padding: 10px;">
 								<div class="col-md">
 									<a href="single.php?masanpham='.$row["masp"].'"><img src="images/'.$row["hinhanh"].'" alt="" /></a>
-									<div class="top-content">';
-										echo "<h5>".$row['tensp']."</h5>";
+									<div class="top-content">
+                                        <h5>'.$row["tensp"].'</h5>';
 										echo '<div class="white clearfix">
 												<a href="#" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">ADD TO CART</a>';
 											echo "<div class='price'>".$row["giatien"]." VND</div>";
@@ -104,12 +104,12 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							echo '<div class="col-md-3" style="padding: 10px;">
 								<div class="col-md">
 									<a href="single.php?masanpham='.$row["masp"].'"><img src="images/'.$row["hinhanh"].'" alt="" /></a>
-									<div class="top-content">';
-										echo "<h5>".$row['tensp']."</h5>";
-										echo '<div class="white clearfix">
-												<a href="#" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">ADD TO CART</a>';
-											echo "<div class='price'>".$row["giatien"]." VND</div>";
-										echo "</div>
+                                    <div class="top-content">
+                                    <h5>'.$row["tensp"].'</h5></a>';
+                                    echo '<div class="white clearfix">
+                                            <a href="#" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">ADD TO CART</a>';
+                                        echo "<div class='price'>".$row["giatien"]." VND</div>";
+                                    echo "</div>
 									</div>
 								</div>
 							</div>";
@@ -130,8 +130,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 							echo '<div class="col-md-3" style="padding: 10px;">
 								<div class="col-md">
 									<a href="single.php?masanpham='.$row["masp"].'"><img src="images/'.$row["hinhanh"].'" alt="" /></a>
-									<div class="top-content">';
-										echo "<h5>".$row['tensp']."</h5>";
+                                    <div class="top-content">
+                                    <h5>'.$row["tensp"].'</h5>';
 										echo '<div class="white clearfix">
 												<a href="#" class="hvr-shutter-in-vertical hvr-shutter-in-vertical2">ADD TO CART</a>';
 											echo "<div class='price'>".$row["giatien"]." VND</div>";
